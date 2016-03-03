024de98479de520c34f310702a660cf02f63b39a
Aslonski <slonski.andrey@gmail.com>
2016-03-03 00:12:55 -0600

    Add 4.6.5 solution file

---
+++ week-4/concatenate-arrays/my_solution.rb
@@ -0,0 +1,11 @@
+# Concatenate Two Arrays
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+def array_concat(array_1, array_2)
+ array_1 + array_2
+end
+p array_concat([22,33,44], ["I'M", "MAKING", "PROGRESS"])
